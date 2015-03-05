07bf50630243f5b4442d5dac962b73f35214e14b
K-2052 <k@2052.me>
2015-03-05 13:42:46 -0800

    added a send_file helper

--- lib/eldr/responders.rb
+++ lib/eldr/responders.rb
@@ -47,5 +47,28 @@ module Eldr
       klass.use Rack::Accept if klass.respond_to? :use
       klass.include Eldr::Rendering unless klass.instance_methods.include? :render
     end
+
+    def send_file(local_file, status: 200, disposition: nil, filename: nil, dir: 'public')
+      dir = self.configuration.public_dir if self.configuration.public_dir
+      lp = local_file[0] == File::SEPARATOR ? local_file : File.expand_path(File.join(dir, local_file))
+      halt 404 unless File.exist? lp
+
+      response = Response.new
+      response.set_status = status
+
+      # Content-Type
+      response['Content-Type'] = (MIME::Types.type_for(File.extname(lp))[0].content_type rescue 'plain/text')
+
+      # Content-Disposition
+      if disposition == :attachment or filename
+        response['Content-Dispostion'] = "attachment; filename=#{(filename or File.basename(lp)}"
+      end
+
+      # Content-Length
+      response['Content-Length'] = File.size(lp)
+
+      response.body << File.read(lp)
+      response
+    end
   end
 end
